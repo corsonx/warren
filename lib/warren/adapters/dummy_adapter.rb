e721ccd6df01a65fa65bc45d164829695e625d3a
Cody Fauser <codyfauser@gmail.com>
2010-01-27 09:47:11 -0500

    add opts to dummy adapter

--- lib/warren/adapters/dummy_adapter.rb
+++ lib/warren/adapters/dummy_adapter.rb
@@ -2,11 +2,11 @@ require "rubygems"
 
 class DummyAdapter < Warren::Queue
 
-  def self.publish queue_name, payload, &blk
+  def self.publish queue_name, payload, options = {}, &blk
     logger.info "publishing #{payload.inspect} to #{queue_name}"
   end
 
-  def self.subscribe queue_name, &block
+  def self.subscribe queue_name, options = {}, &block
     logger.info "subscribing to #{queue_name}"
   end
 end
