d6abb46c6779c6869c15a8ec988a9790bac5c261
Maximilian Schöfmann <schoefmann@gmail.com>
2011-09-29 20:37:51 +0200

    adding the new methods also to the DummyAdapter

--- lib/warren/adapters/dummy_adapter.rb
+++ lib/warren/adapters/dummy_adapter.rb
@@ -7,4 +7,17 @@ class DummyAdapter < Warren::Queue
   def self.subscribe queue_name, options = {}, &block
     logger.info "subscribing to #{queue_name}"
   end
+
+  def self.stay_connected &blk
+    logger.info "staying connected (begin)"
+    blk.arity == 1 ? blk.call(self) : blk.call
+    logger.info "staying connected (end)"
+  end
+
+  def self.client
+    nil
+  end
+
+  def self.reset
+  end
 end
